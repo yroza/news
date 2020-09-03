5542ccfb84c34f956421b83564a4b64398aabcb3
unknown <yashoza124@gmail.com>
2020-09-03 15:59:33 +0530

    about us

--- lib/ui/aboutus.dart
+++ lib/ui/aboutus.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:news/ui/WebView.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class AboutUs extends StatefulWidget {
   @override
@@ -6,10 +10,25 @@ class AboutUs extends StatefulWidget {
 }
 
 class aboutssState extends State<AboutUs> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Material(child: Scaffold());
+    return SafeArea(
+      child: Material(
+          child: Scaffold(
+        body: Container(
+            child: WebView(
+          initialUrl: "http://peoplenews.asia/about-us?pvappv=1",
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+        )),
+      )),
+    );
     // throw UnimplementedError();
   }
 }
